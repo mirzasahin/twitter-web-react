b4d5d03e3695326445d10679a2714b10670dce6c
Mirza Sahin <salihmirza123@gmail.com>
2024-02-17 01:54:10 +0300

    Add sidebar layouts

---
+++ src/layouts/main/rightbar/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const RightBar = () => {
+  return (
+    <aside className='w-[350px] mr-2.5'>
+        sag sidebar!
+    </aside>
+  )
+}
+
+export default RightBar
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React, { Children } from "react";
+import MainLayout from "../../layouts/main";
 
 const Home = () => {
   return (
-    <div className='bg-red-200 text-red-600 p-5 flex items-center justify-center'>Home Component</div>
-  )
-}
+    /* <div className='bg-red-200 text-red-600 p-5 flex items-center justify-center'>Home Component</div> */
+    <div>home component</div>
+  );
+};
 
-export default Home
+export default Home;
